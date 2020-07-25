0aa71a71cef2763937ed11734c38224695313a48
Tuhin Mitra <tuhinmitra190221@gmail.com>
2020-07-25 11:36:04 +0530

    Python implementation for Levenshtein Distance.

--- Levenshtein_distance.py
+++ Levenshtein_distance.py
@@ -1,9 +1,14 @@
 """
-In information theory, linguistics and computer science, the Levenshtein distance is a string metric for measuring the difference between two sequences. Informally, the Levenshtein distance between two words is the minimum number of single-character edits (insertions, deletions or substitutions) required to change one word into the other. It is named after the Soviet mathematician Vladimir Levenshtein, who considered this distance in 1965.[1]
+In information theory, linguistics and computer science, the Levenshtein distance is a string metric for measuring the
+difference between two sequences. Informally, the Levenshtein distance between two words is the minimum number of
+single-character edits (insertions, deletions or substitutions) required to change one word into the other.
+It is named after the Soviet mathematician Vladimir Levenshtein, who considered this distance in 1965.[1]
 
-Levenshtein distance may also be referred to as edit distance, although that term may also denote a larger family of distance metrics known collectively as edit distance.[2]:32 It is closely related to pairwise string alignments.
+Levenshtein distance may also be referred to as edit distance, although that term may also denote a larger family of
+distance metrics known collectively as edit distance.[2]:32 It is closely related to pairwise string alignments.
 
 (source: wikipedia)
+And this webpage helped me a lot: https://medium.com/@ethannam/understanding-the-levenshtein-distance-equation-for-beginners-c4285a5604f0
 """
 import pprint
 
