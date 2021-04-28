64b042e65cb0d5bc3570fb2b8dc7a0f0c7e96a3c
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-04-28 13:39:37 +0530

    no of open doors

---
+++ mathematical/noOfopendoors.py
@@ -0,0 +1,16 @@
+"""
+problem link: https://practice.geeksforgeeks.org/problems/number-of-open-doors1552/1
+"""
+class Solution:
+    def noOfOpenDoors(self, N):
+        pass
+
+
+import math
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        print(ob.noOfOpenDoors(N))
+
