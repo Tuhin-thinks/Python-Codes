490fcf5377699da92be1f968717f194a6dc9fba1
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-05-24 20:33:58 +0530

    rounding off methods # numerical methods reason updated

--- mathematical/calculate_finite_points.py
+++ mathematical/calculate_finite_points.py
@@ -17,6 +17,7 @@ def round_off(number:float, place:int):
         pass
     
     elif int(m_1_place) == 5:
+        # Take a look at the bottom of the file for more information (why this step is nescessary)
         if int(m_place) % 2 == 0:  # number is even
             pass
         else:  # number is odd
@@ -34,12 +35,18 @@ def round_off_significant_digit(number, signification_digit):
     else:
         return number
 
-dec_place = 2
-nr, dec_place = 23.246, 2
+nr, dec_place = 23.245, 2
 # nr, dec_place = 0.025355, 4
 # nr, dec_place = 12.372, 3
 # nr, dec_place = 6300, 3
-# ans = round_off(nr, dec_place)
-# print(f"rounded off to {dec_place} places: {ans}")
-ans = round_off_significant_digit(nr, dec_place)
-print(f"Rounded off to {dec_place} significant digits:", ans)
+ans = round_off(nr, dec_place)
+print(f"rounded off to {dec_place} places: {ans}")
+# ans = round_off_significant_digit(nr, dec_place)
+# print(f"Rounded off to {dec_place} significant digits:", ans)
+
+
+# Note: If the digit at N+1 place is 5 and the digit at digit at N place is even we keep the digit as it is.
+# else, for the N place digit as odd, we increase it by 1.
+# reason: This convention is nescessary to keep a set of numbers  as "balanced" as possible. i.e. if you round down for
+# digits from 1-5 the (five cases) and up for 6-9 (4 cases), the sum of the resulting numbers will be on average, lower than the sum
+# of the unrounded terms.
